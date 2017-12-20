3adc2362b4d865175a29a8d83119479159db3e2c
Bortsukh <bortsukh.stanislav@yandex.ru>
2017-12-20 22:18:35 +0300

    поработал над 5.2(1)

--- js/map.js
+++ js/map.js
@@ -49,17 +49,65 @@
     }
   };
   var setupIsFinished = false;
-  var pinMain = document.querySelector('.map__pin--main');
-  pinMain.addEventListener('mouseup', function () {
-    if (!setupIsFinished) {
-      window.pin.add();
-      showMap();
-      window.form.open();
-      window.form.activate();
-      addPinsListener();
-      setupIsFinished = true;
-    }
+
+  mapPins.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      mapPins.style.top = (mapPins.offsetTop - shift.y) + 'px';
+      mapPins.style.left = (mapPins.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      window.addEventListener('mouseup', function () {
+        if (!setupIsFinished) {
+          window.pin.add();
+          showMap();
+          window.form.open();
+          window.form.activate();
+          addPinsListener();
+          setupIsFinished = true;
+        }
+      });
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
+
+
+
+  // var pinMain = document.querySelector('.map__pin--main');
+  // pinMain.addEventListener('mouseup', function () {
+  //   if (!setupIsFinished) {
+  //     window.pin.add();
+  //     showMap();
+  //     window.form.open();
+  //     window.form.activate();
+  //     addPinsListener();
+  //     setupIsFinished = true;
+  //   }
+  // });
   window.map = {
     template: template,
     mapPins: mapPins,
